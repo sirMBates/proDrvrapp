54dbaef7fef1655141ac82ca9e97827278cd5668
sirMBates <marvbatesjr@gmail.com>
2025-05-21 13:15:53 -0400

    Added background color to text on reset page. slight changes on other files.

--- public/views/reset.view.php
+++ public/views/reset.view.php
@@ -4,8 +4,8 @@
         include "includes/errormsgs.php";
 ?>
     <main class="w-50 my-5 align-self-center">
-        <h1 class="text-center text-light font-monospace"><u>Reset your password here!</u></h1>
-        <p class="h5 text-center text-light">You will receive an email with further instructions on resetting your password.</p>
+        <h1 class="text-center text-light text-bg-dark font-monospace"><u>Reset your password here!</u></h1>
+        <p class="h5 text-center text-light text-bg-dark">You will receive an email with further instructions on resetting your password.</p>
         <form id="resetpswd" action="../../app/controllers/reset-password.php" method="POST" class="needs-validation" novalidate>
             <div class="form-floating row mb-2">
                 <input id="email-for-pswdreset" type="email" inputmode="email" class="form-control" placeholder="Please enter your email" required>
@@ -13,7 +13,7 @@
             </div>
             <div class="row mb-2">
                 <button type="button" class="btn btn-lg btn-primary my-2 p-3" name="reset-pswd">Send email</button>
-                <button type="button" class="btn btn-lg btn-primary my-2 p-3" name="cancel">Go back</button>
+                <button type="button" class="btn btn-lg btn-primary my-2" name="cancel">Go back</button>
             </div>
         </form>
     </main> 
