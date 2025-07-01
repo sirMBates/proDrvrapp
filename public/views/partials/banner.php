e8a06a5a47ea7683290edbaf601ee01a68fbf0cf
sirMBates <marvbatesjr@gmail.com>
2025-06-30 21:15:33 -0400

    Updated the design & feel of the home page. Also, redesign the banner look.

--- public/views/partials/banner.php
+++ public/views/partials/banner.php
@@ -1,8 +1,8 @@
-<header class="container-fluid d-flex flex-row justify-content-between mb-2 bg-btd-gray-silver border-bottom border-1 border-black">
-        <div class="d-flex flex-column justify-content-start">                                
-                <img src="../images-videos/prodrvrbkgd.png" class="align-self-start m-2 img-fluid" width="250" id="logo" alt="companylogo">                       
+<header class="container-fluid d-inline-flex justify-content-between mb-2 bg-btd-gray-silver border-bottom border-1 border-black">
+        <div class="flex-shrink-0" style="width: 20%;">                                
+                <img src="../images-videos/prodrvrbkgd.png" class="img-fluid m-3" width="250" id="logo" alt="companylogo">                       
         </div>
-        <div class="d-flex flex-column justify-content-start align-items-end">
+        <div class="d-flex flex-column flex-grow-1 ms-3 justify-content-start align-items-end">
                 <div id="clock_container" class="z-3 d-flex flex-column text-capitalize">
                         <div id="dateCon" class="container-sm d-flex flex-row" style="margin-right: -5%;">
                                 <div class="text-btd-blue-bright mx-1"></div>
