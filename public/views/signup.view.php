c992e50af861ba4c7163ba663115e0bd1b802998
sirMBates <marvbatesjr@gmail.com>
2025-07-12 03:57:18 -0400

    Working on finishing the reset password functionality.

--- public/views/signup.view.php
+++ public/views/signup.view.php
@@ -9,22 +9,22 @@ include "includes/flashmessage.php";
                 <form id="signUpAcct" class="needs-validation" action="" method="POST" novalidate>
                         <div class="input-group input-group-lg px-3 my-3 d-flex justify-content-center flex-column">
                                 <div class="input-group my-1">
-                                        <input type="text" id="username" class="form-control fs-4 rounded-2" name="username" placeholder="Username" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Username" data-bs-content="Must contain a minimum of 5 characters with atleast 1 (U)ppercase letter, 1  (l)owercase letter & a number." data-bs-placement="top" data-bs-trigger="focus" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).\S{4,}$" required>
+                                        <input type="text" id="username" class="form-control fs-4 rounded-2" name="username" placeholder="Username" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Username" data-bs-content="Must contain a minimum of 5 characters with atleast 1 (U)ppercase letter, 1  (l)owercase letter & a number." data-bs-placement="bottom" data-bs-trigger="focus" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).\S{4,}$" required>
                                         <div class="invalid-feedback">Please enter a username.</div>
                                 </div>
                                 
                                 <div class="input-group my-1">
-                                        <input id="userEmail" type="email" inputmode="email" class="form-control fs-4 rounded-2" name="email" placeholder="Email Address" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Email" data-bs-content="Please enter your email address" data-bs-placement="right" data-bs-trigger="focus" required>
+                                        <input id="userEmail" type="email" inputmode="email" class="form-control fs-4 rounded-2" name="email" placeholder="Email Address" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Email" data-bs-content="Please enter your email address" data-bs-placement="bottom" data-bs-trigger="focus" required>
                                         <div class="invalid-feedback">Please enter your email.</div>
                                 </div>
                                 
                                 <div class="input-group my-1">
-                                        <input type="password" id="password" class="form-control fs-4" name="password" placeholder="Password" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Password" data-bs-content="Must contain a minimum of 8 characters with atleast 1 (U)ppercase letter, 1 (l)owercase letter, 1 number and 1 special character. (i.e. !@#%&_)" data-bs-placement="right" data-bs-trigger="focus" pattern="^(?=.*[a-z])(?=.*[A-Z]*[A-Z])(?=.*[0-9])(?=.*[!@#%&_]).\S{7,}$" required><span class="input-group-text rounded-end" id="pwd-icon-click" aria-describedby="password"><i class="fa-solid fa-eye" id="pwd-icon"></i></span>
+                                        <input type="password" id="password" class="form-control fs-4" name="password" placeholder="Password" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Password" data-bs-content="Must contain a minimum of 8 characters with atleast 1 (U)ppercase letter, 1 (l)owercase letter, 1 number and 1 special character. (i.e. !@#%&_)" data-bs-placement="bottom" data-bs-trigger="focus" pattern="^(?=.*[a-z])(?=.*[A-Z]*[A-Z])(?=.*[0-9])(?=.*[!@#%&_]).\S{7,}$" required><span class="input-group-text rounded-end" id="pwd-icon-click" aria-describedby="password"><i class="fa-solid fa-eye" id="pwd-icon"></i></span>
                                         <div class="invalid-feedback">Please enter a password</div>
                                 </div>          
                                 
                                 <div class="input-group my-1">
-                                        <input type="password" id="confirmPassword" class="form-control fs-4" name="confirmPassword" placeholder="Confirm Password" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Confirm password" data-bs-content="Re-enter password" data-bs-placement="right" data-bs-trigger="focus" required><span class="input-group-text rounded-end" aria-describedby="confirmPassword"><i class="fa-solid fa-eye" id="con-pwd-icon"></i></span>
+                                        <input type="password" id="confirmPassword" class="form-control fs-4" name="confirmPassword" placeholder="Confirm Password" data-bs-container="body" data-bs-toggle="popover" data-bs-title="Confirm password" data-bs-content="Re-enter password" data-bs-placement="bottom" data-bs-trigger="focus" required><span class="input-group-text rounded-end" aria-describedby="confirmPassword"><i class="fa-solid fa-eye" id="con-pwd-icon"></i></span>
                                         <div class="invalid-feedback">Re-type password exactly.</div>
                                 </div>
 
