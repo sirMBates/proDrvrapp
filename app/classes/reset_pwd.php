6540a0ea65e624bd553db8e96f614eac743d4ca6
sirMBates <marvbatesjr@gmail.com>
2025-07-14 17:19:13 -0400

    Still refactoring the token functionality.

--- app/classes/reset_pwd.php
+++ app/classes/reset_pwd.php
@@ -1,3 +1,50 @@
 <?php
 
-class ResetPwdContr extends ResetPwd {}
+use core\Flash;
+
+class ResetPwdContr extends ResetPwd {
+    private $token;
+    private $tokenExp;
+
+    public __construct($token, $tokenExp) {
+        $this->token = $token;
+        $this->tokenExp = $tokenExp;
+    }
+
+    public function isTokenExpired() {
+        $alert = new Flash();
+        if($this->checkTokenExpiration($this->token) === true) {
+            $alert::setMsg('validate', 'Token has expired! Please click button to generate a new token.');
+            header("Location: /reset?validate=expired");
+            exit();
+        }
+    }
+
+    public function createNewToken() {
+        $alert = new Flash();
+        if ($this->getNewToken($this->token, $this->tokenExp) === false) {
+            $alert::setMsg('error', 'There was a problem. Please, try again!');
+            header("Location: /reset?error=not+valid");
+            exit();
+        } else {
+            $mail = require_once home_path("mail/emailSetup.php");
+            $mail->setFrom('noreply@prodriver.local', 'Help Desk');
+            $mail->addAddress($this->email);
+            $mail->Subject = "Forget Password";
+            $mail->Body = <<<END
+
+                    Click <a href="http://prodriver.local/reset?token=$this->token_hash">here</a> to forget password.
+
+                    END;
+            try {
+                $mail->send();
+            } catch (Exception $e) {
+                $alert = new Flash(); //remove(comment out if need to check mailer errors).
+                //echo "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
+                $alert::setMsg('danger', "Message not sent. Try again. {$mail->ErrorInfo}");
+                header("Location: /forget?danger=system+error");
+                exit();
+            }
+        }
+    }
+}
--- app/controllers/resetpw.php
+++ app/controllers/resetpw.php
@@ -7,17 +7,30 @@ if (session_status() !== 2) {
 }
 
 if (isset($_GET['token'])) {
-    $token = $_GET['token_hash'];
-    $token_hash = hash("sha256", $token);
-} elseif (isset($_POST['reset-pswd'])) {
-            // Get the token from the queryString
-            $token = $_GET['token'];
-            $sanitizedToken = filter_var($token, FILTER_SANITIZE_STRING);
-            $token_hash = hash("sha256", $sanitizedToken);
-            // Getting the info from the form using POST method from the name attribute.
-            $password = htmlspecialchars($_POST['password']);
-            // Instantiate the add user controller class. ↓
+    // Get the token from the queryString
+    $token = $_GET['token'];
+    $sanitizedToken = filter_var($token, FILTER_SANITIZE_STRING);
+    $token_hash = hash("sha256", $sanitizedToken);
+    include_once base_path("app/models/database.php");
+    include_once base_path("app/models/resetpwdmeth.php");
+    include_once base_path("app/classes/reset_pwd.php");
+    $driverToken = new ResetPwdContr($token_hash);
+    $driverToken->isTokenExpired();
+    $alert::setMsg('validate', 'Please fill out the form below to complete password reset.');
+    header("Location: /reset?validate=cleared");
+    exit();
+} elseif (isset($_POST['generate'])) {
+            // Create new token to send to email of driver
+            $token = bin2hex(random_bytes(16));
+            $token_hash = hash('sha256', $token);
+            $tokenExpTime = date("Y-m-d H:i:s", time() + 60 * 30); // 30 minutes expiration time
+            // Instantiate the reset token controller class. ↓
             include_once base_path("app/models/database.php");
             include_once base_path("app/models/resetpwdmeth.php");
             include_once base_path("app/classes/reset_pwd.php");
+            $newDriverToken = new ResetPwdContr($token_hash, $token);
+            $newDriverToken->createNewToken();
+            $alert::setMsg('info', 'Please check your inbox for reset link.');
+            header("Location: /reset?info=sent+email");
+            exit();
 }
