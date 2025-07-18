946d7d11bebdd07751c0007d6c1fa8dfe3963741
sirMBates <marvbatesjr@gmail.com>
2025-07-18 13:33:11 -0400

    Adjusted application time zone to be concurrent with device. Updated reset and forget files (still working on the expire time).

--- app/controllers/forgetpw.php
+++ app/controllers/forgetpw.php
@@ -11,13 +11,12 @@ if (isset($_POST['forget-pswd'])) {
     $email = htmlspecialchars($_POST['email']);
     $setToken = bin2hex(random_bytes(16));
     $token = hash('sha256', $setToken);
-    $token_time_format = date('Y-m-d H:i:s');
-    $token_expires = strtotime($token_time_format) + (60 * 30); // 30 minutes expiration
-    $tokenExpTime = date("Y-m-d H:i:s", $token_expires);
+    // The time() method gives you the current time in seconds.
+    $token_expires = date('Y-m-d H:i:s', time() + 60 * 30); // 30 minutes expiration
     include_once base_path("app/models/database.php");
     include_once base_path("app/models/forgetpwdmeth.php");
     include_once base_path("app/classes/forget_pswd.php");
-    $startReset = new ForgetPswdContr($token, $tokenExpTime, $email);
+    $startReset = new ForgetPswdContr($token, $token_expires, $email);
     $startReset->addTokenAndExpireTime();
     $startReset->sendForgetEmail();
     $alert::setMsg('info', 'Email sent! Please check your inbox to complete the reset.');
