e75463bc7df9d90e30e26f6dce87416ded972062
sirMBates <marvbatesjr@gmail.com>
2025-07-20 00:28:34 -0400

    Refactored the add user controller and dbmeth.

--- app/models/addusermeth.php
+++ app/models/addusermeth.php
@@ -3,7 +3,7 @@
 use core\Flash;
 
 class AddedDrvr extends ConnectDatabase {
-    protected function setDriver($username, $email, $password, $firstname, $lastname, $mobileNum, $birthdate) {
+    protected function setDriver($username, $email, $password) {
         $alert = new Flash();
         $sql = "INSERT INTO driver (
                 username, email, password, firstName, lastName, mobileNumber, birthdate) 
@@ -11,72 +11,54 @@ class AddedDrvr extends ConnectDatabase {
         $stmt = $this->connect()->prepare($sql);
 
         $hashPsW = password_hash($password, PASSWORD_BCRYPT);
+        $tmpFirstName = '';
+        $tmpLastName = '';
+        $tmpMobileNum = '';
+        $tmpBirthDate = NULL;
+        $stmt->bindParam(1, $username);
+        $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $hashPsW);
+        $stmt->bindParam(4, $tmpFirstName);
+        $stmt->bindParam(5, $tmpLastName);
+        $stmt->bindParam(6, $tmpMobileNum);
+        $stmt->bindParam(7, $tmpBirthDate);
 
-        if (!$stmt->execute(array($username, $email, $hashPsW, $firstname, $lastname, $mobileNum, $birthdate))) {
+        $result = $stmt->execute();
+
+        if (!$result) {
             $alert::setMsg('error', 'An unexpected error occurred. Please try again.');
             header("Location: /?error=try+again"); //stmtfailed
             exit();
         }
 
-        /*$drvrIdMainTab = $this->connect()->lastInsertId();
-        $sql2 = "INSERT INTO pwdreset (resetid, email)
-                VALUES (:resetid, :email)";
+        $token = '';
+        $tokenExpTime = NULL;
+        $sql2 = "INSERT INTO pwdreset (email, token, tokenExpTime)
+                VALUES (?,?,?)";
         $stmt2 = $this->connect()->prepare($sql2);
-        $result = $stmt2->execute([
-            ':resetid' => $drvrIdMainTab,
-            ':email' => $email
-        ]);
+        $stmt2->bindParam(1, $email);
+        $stmt2->bindParam(2, $token);
+        $stmt2->bindParam(3, $tokenExpTime);
+        
+        $result2 = $stmt2->execute();
 
-        if (!$result) {
-            $alert::setMsg('error', 'There was a problem. Try again.');
+        if (!$result2) {
+            $alert::setMsg('error', 'An unexpected error occurred. Please try again.');
             header("Location: /error?=try+again");  //stmtfailed
             exit();
-        }*/
+        }
     }
 
-    /*protected function addToReset($email) {
+    protected function checkDriver($username, $email) {
         $alert = new Flash();
-        $sql = "INSERT INTO pwdreset (resetid, drvr_email)
-                VALUES (:resetid, :email)";
+        $sql = "SELECT driverid FROM driver
+                WHERE username = ? OR email = ?";
         $stmt = $this->connect()->prepare($sql);
-        $result = $stmt->execute([
-            ':resetid' => $drvrIdMainTab,
-            ':email' => $email
-        ]);
+        $stmt->bindParam(1, $username);
+        $stmt->bindParam(2, $email);
+        $result = $stmt->execute();
 
         if (!$result) {
-            $alert::setMsg('error', 'There was a problem. Try again.');
-            header("Location: /?error=try+again");
-            exit();
-        }
-    }*/
-
-    /*protected function checkIfEmailExist($email) {
-        $alert = new Flash();
-        $sql = "SELECT drvr_email FROM pwdreset 
-                WHERE email = ?";
-        $stmt =$this->connect()->prepare($sql);
-        if (!$stmt->execute([$email])) {
-            $stmt = null;
-            $alert::setMsg('error', 'An unexpected error occurred. Please try again.');
-            header("Location: /forget?error=try+again"); //stmtfailed
-            exit();
-        } 
- 
-        $resultCheck;
-        if ($stmt->rowCount() === 0) {
-            $resultCheck = false;
-        } else {
-            $resultCheck = true;
-        }
-        return $resultCheck;
-    }*/
-
-    protected function checkDriver($username, $email) {
-        $stmt =$this->connect()->prepare('SELECT driverid FROM driver WHERE username = ? OR email = ?;');
-        if (!$stmt->execute(array($username, $email))) {
-            $alert = new Flash();
-            $stmt = null;
             $alert::setMsg('error', 'An unexpected error occurred. Please try again.');
             header("Location: /?error=try+again"); //stmtfailed
             exit();
