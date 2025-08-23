0fe883bcccca0d65f71525c8158c8ff1c0ba74a2
sirMBates <marvbatesjr@gmail.com>
2025-08-23 15:46:02 -0400

    Refactored the inside controllers to add authentication before completing submission.

--- app/controllers/compresetproc.php
+++ app/controllers/compresetproc.php
@@ -6,20 +6,27 @@ if (session_status() !== 2) {
     session_start();
 }
 
-if (isset($_POST['reset-pswd'])) {
-    // Create new token to send to email of driver
-    $token = htmlspecialchars($_POST['resetToken']);
-    $password = htmlspecialchars($_POST['password']);
-    //echo $token;
-    //echo $password;
-    // Instantiate the reset token controller class. ↓
-    include_once base_path("app/models/comppwdmeth.php");
-    include_once base_path("app/classes/comp_pwd_process.php");
-    $createNewPwd = new CompleteResetContr($token, $password);
-    $createNewPwd->changeDrvrPassword();
-    $createNewPwd->hasTokenCleared();
-    $alert::setMsg('success', 'Please log in to your account.');
-    header("Location: /signin?success=reset+complete");
+$formToken = htmlspecialchars(trim($_POST['drvrtoken']));
+if ($formToken === $_SESSION['drvr_token']) {
+    if (isset($_POST['reset-pswd'])) {
+        // Create new token to send to email of driver
+        $token = htmlspecialchars($_POST['resetToken']);
+        $password = htmlspecialchars($_POST['password']);
+        //echo $token;
+        //echo $password;
+        // Instantiate the reset token controller class. ↓
+        include_once base_path("app/models/comppwdmeth.php");
+        include_once base_path("app/classes/comp_pwd_process.php");
+        $createNewPwd = new CompleteResetContr($token, $password);
+        $createNewPwd->changeDrvrPassword();
+        $createNewPwd->hasTokenCleared();
+        $alert::setMsg('success', 'Please log in to your account.');
+        header("Location: /signin?success=reset+complete");
+        exit();
+    }
+} else {
+    $alert::setMsg('danger', 'Please retry your request.');
+    header("Location: /signup?danger=try+again");
     exit();
 }
 ?>
