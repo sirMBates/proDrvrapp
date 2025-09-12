a82fac84dee406537f524df21cef24162fa053d1
sirMBates <70979923+sirMBates@users.noreply.github.com>
2025-09-12 12:57:28 -0400

    Update register.php

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -1,15 +1,5 @@
 <?php
 
-if (!isset($_SESSION['user_name'])) {
-    header("Location: /signup");
-    exit();
-}
-
-if (isset($_SESSION['driver_id'])) {
-    header("Location: /");
-    exit();
-}
-
 view('register.view.php', ['title' => 'Pro Driver - Register',]);
 
 ?>
