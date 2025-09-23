e2c1cb09021df533abf736964ffec7cb05f86f36
sirMBates <marvbatesjr@gmail.com>
2025-09-23 02:46:48 -0400

    Refactored and updated the profile picture system. Added an secondary location for driver profile uploads/downloads files.

--- public/views/partials/nav.php
+++ public/views/partials/nav.php
@@ -2,7 +2,7 @@
         <div class="offcanvas-header bg-besttrailsclr">
                 <div id="profilecon" style="width: 60px; height: 60px;" class="border border-2 border-primary rounded d-inline-block me-2">                        
                         <label for="profile-upload"><img id="profile-pic" src="../dist/images-videos/logoandicons/photo-camera-interface-symbol-for-button.png" alt="N/A" width="50" height="50" class="mx-1 my-1"></label>
-                        <input type="file" id="profile-upload" accept="image/jpg, image/jpeg, image/png, image/gif" capture="user">
+                        <input type="file" id="profile-upload" accept="image/jpg, image/jpeg, image/png, image/gif">
                 </div>
                 <h5 class="offcanvas-title text-light" id="useraccessLabel"></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="close"></button>
