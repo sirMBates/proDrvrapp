5e81591aba90ad111d10e3b567d13a49a5209fd7
sirMBates <70979923+sirMBates@users.noreply.github.com>
2025-09-24 21:27:59 -0400

    Update assignmentmodel.php

--- app/models/assignmentmodel.php
+++ app/models/assignmentmodel.php
@@ -1,15 +1,12 @@
 <?php
 
 use core\Database;
+use core\Alert;
 
 class Assignment {
-    protected $db;
-
-    public function __construct() {
-        $this->db = new Database();
-    }
-
     public function insertAssignment(array $data): bool {
+      $db = new Database();
+      $alert = new Alert();
         $sql = "INSERT INTO work_orders (vehicle_id, operator_id, name, num_of_coaches,
                 start_date_time, spot_time, leave_date_time, return_date_time, actual_drop_time,
                 end_date_time, actual_end_time, total_job_hrs, driving_time, origin, destination,
@@ -17,11 +14,8 @@ class Assignment {
                 contact_name, contact_mobile, location_pickup_details, destination_location_details,
                 signature_required, signature, driver_notes
             ) 
-            VALUES (:vehicle_id, :operator_id, :name, :num_of_coaches, :start_date_time, :spot_time, :leave_date_time,
-                    :return_date_time, :actual_drop_time, :end_date_time, :actual_end_time, :total_job_hrs, :driving_time,
-                    :origin, :destination, :group_name, :group_leader, :group_leader_mobile, :customer_name,
-                    :customer_phone, :contact_name, :contact_mobile, :location_pickup_details, :destination_location_details,
-                    :signature_required, :signature, :driver_notes)";
+            VALUES (?, ?, ?, ?, ?, ?, ?,
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     }
 }
 ?>
